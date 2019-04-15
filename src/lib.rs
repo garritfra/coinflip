b1ddc2ade9d17f5c5bdc4f6902a31b6cbcc8649b
garritfra <garritfranke@gmail.com>
2019-04-15 18:37:19 +0200

    Fix random

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,5 @@
 extern crate rand;
-use rand::distributions::Range;
 
 pub fn flip() -> bool {
-    let step = Range::new(0, 1);
-    let mut rng = rand::thread_rng();
-    let choice = step.ind_sample(&mut rng);
-    match choice {
-        1 => true,
-        0 => false,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let coin = flip();
-        assert!(coin == true || coin == false);
-    }
+    rand::random::<bool>()
 }
